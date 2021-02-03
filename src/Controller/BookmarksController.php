4a25bb2a55eafce2fa919395e2f946226d94b29c
takumimurakami <78392283+takumimurakami@users.noreply.github.com>
2021-02-03 12:39:05 +0900

    first

---
+++ src/Controller/BookmarksController.php
@@ -0,0 +1,154 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Bookmarks Controller
+ *
+ * @property \App\Model\Table\BookmarksTable $Bookmarks
+ *
+ * @method \App\Model\Entity\Bookmark[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class BookmarksController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function index()
+    {
+        $this->paginate = [
+            'conditions' => [
+                'Bookmarks.user_id' => $this->Auth->user('id'),
+            ]
+        ];
+        $this->set('bookmarks', $this->paginate($this->Bookmarks));
+        $this->set('_serialize', ['bookmarks']);
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Bookmark id.
+     * @return \Cake\Http\Response|null
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $bookmark = $this->Bookmarks->get($id, [
+            'contain' => ['Users', 'Tags'],
+        ]);
+
+        $this->set('bookmark', $bookmark);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $bookmark = $this->Bookmarks->newEntity();
+        if ($this->request->is('post')) {
+            $bookmark = $this->Bookmarks->patchEntity($bookmark, $this->request->getData());
+            $bookmark->user_id = $this->Auth->user('id');
+            if ($this->Bookmarks->save($bookmark)) {
+                $this->Flash->success('ブックマークを保存しました。');
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error('ブックマークは保存できませんでした。もう一度お試しください。');
+        }
+        $tags = $this->Bookmarks->Tags->find('list');
+        $this->set(compact('bookmark', 'tags'));
+        $this->set('_serialize', ['bookmark']);
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Bookmark id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $bookmark = $this->Bookmarks->get($id, [
+            'contain' => ['Tags']
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $bookmark = $this->Bookmarks->patchEntity($bookmark, $this->request->getData());
+            $bookmark->user_id = $this->Auth->user('id');
+            if ($this->Bookmarks->save($bookmark)) {
+                $this->Flash->success('ブックマークを保存しました。');
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error('ブックマークは保存できませんでした。もう一度お試しください。');
+        }
+        $tags = $this->Bookmarks->Tags->find('list');
+        $this->set(compact('bookmark', 'tags'));
+        $this->set('_serialize', ['bookmark']);
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Bookmark id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $bookmark = $this->Bookmarks->get($id);
+        if ($this->Bookmarks->delete($bookmark)) {
+            $this->Flash->success(__('The bookmark has been deleted.'));
+        } else {
+            $this->Flash->error(__('The bookmark could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+
+    public function tags()
+    {
+        // CakePHP によって提供された 'pass' キーは全ての
+        // リクエストにある渡された URL のパスセグメントです。
+        $tags = $this->request->getParam('pass');
+
+        // タグ付きのブックマークを探すために BookmarksTable を使用
+        $bookmarks = $this->Bookmarks->find('tagged', [
+            'tags' => $tags
+        ]);
+
+        // ビューテンプレートに変数を渡します
+        $this->set([
+            'bookmarks' => $bookmarks,
+            'tags' => $tags
+        ]);
+    }
+
+    public function isAuthorized($user)
+    {
+        $action = $this->request->getParam('action');
+
+        // add と index アクションは常に許可します。
+        if (in_array($action, ['index', 'add', 'tags'])) {
+            return true;
+        }
+        // その他のすべてのアクションは、id を必要とします。
+        if (!$this->request->getParam('pass.0')) {
+            return false;
+        }
+
+        // ブックマークが現在のユーザーに属するかどうかをチェック
+        $id = $this->request->getParam('pass.0');
+        $bookmark = $this->Bookmarks->get($id);
+        if ($bookmark->user_id == $user['id']) {
+            return true;
+        }
+        return parent::isAuthorized($user);
+    }
+}
---
+++ src/Template/Bookmarks/tags.ctp
@@ -0,0 +1,17 @@
+<h1>
+    Bookmarks tagged with
+    <?= $this->Text->toList(h($tags)) ?>
+</h1>
+
+<section>
+<?php foreach ($bookmarks as $bookmark): ?>
+    <article>
+        <!-- リンクを作成するために HtmlHelper を使用 -->
+        <h4><?= $this->Html->link($bookmark->title, $bookmark->url) ?></h4>
+        <small><?= h($bookmark->url) ?></small>
+
+        <!-- テキストを整形するために TextHelper を使用 -->
+        <?= $this->Text->autoParagraph(h($bookmark->description)) ?>
+    </article>
+<?php endforeach; ?>
+</section>
